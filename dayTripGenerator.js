b9e900c6a9ef14bf42a2195baaf4ab6bb7acbfb2
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-02 15:57:36 -0400

    created randomRestaurant function that chooses a location from a hard coded array of restaurants

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -13,3 +13,13 @@ function randomDestination() {
 // not saved to variable because random will change every execution
 console.log(randomDestination());
 
+function randomRestaurant() {
+    let restaurant = ['bahama breeze', 'olive garden', 'cheesecake factory', 'red lobster', 'yard house '];
+    let chosenRestaurant;
+    for(let i = 0; i < restaurant.length; i++) {
+        let rand = Math.floor(Math.random() * ((restaurant.length - 1) - 0 + 1) + 0);
+        chosenRestaurant = restaurant[rand];
+    }
+    return chosenRestaurant;
+}
+console.log(randomRestaurant());
