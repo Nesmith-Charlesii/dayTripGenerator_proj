905e70dc6b6d3775d51563df80a7772f0ea8cc84
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-04 19:46:51 -0400

    created a greeting alert, a prompt to obtain the user's name, and a reply to start the day trip generator. All random day trip functions; randomDestination, randomRestaurant, randomTransportation, and randomEntertainment, have been condensed to use the loopArray function as a choice selector

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,83 +1,94 @@
 "use strict"; 
 
 let dayTrip = [];
+let destination = ['raleigh', 'durham', 'charlotte', 'wilmington', 'fayetteville', 'myrtle beach'];
+let restaurant = ['bahama breeze', 'olive garden', 'cheesecake factory', 'red lobster', 'yard house '];
+let transporation = ['car', 'bus', 'shared ride', 'tram', 'flight'];
+let entertainment = ['sightsee', 'comedy club', 'museum', 'hike', 'beach'];
+
+//modularized this function to ensure it ONLY loops through an array, but does not push a value to the array.
+function loopArray(inputArray) {
+    let choice;
+    for(let i = 0; i < inputArray.length; i++) {
+        let rand = Math.floor(Math.random() * ((inputArray.length - 1) - 0 + 1) + 0);
+        choice = inputArray[rand];
+    }
+    return choice; 
+}
+//console.log(loopArray(destination));
+
+let greeting = alert("Hello, and welcome to Day Trip Generator!");
+let userFirstName = prompt("Please enter your name");
+let reply = alert("Welcome " + userFirstName + "! Please click ok to generate your random day trip");
 
 function randomDestination() {
-    let destination = ['raleigh', 'durham', 'charlotte', 'wilmington', 'fayetteville', 'myrtle beach'];
-    let chosenDestination;
-    for(let i = 0; i < destination.length; i++) {
-        let rand = Math.floor(Math.random() * ((destination.length - 1) - 0 + 1) + 0);
-        chosenDestination = destination[rand];
-        //console.log(chosenDestination);
-    }
+    let chosenDestination = loopArray(destination);
     dayTrip.push(chosenDestination);
     return chosenDestination;
 }
 // not saved to variable because random will change every execution
-let destination = randomDestination();
+console.log(randomDestination());
 // destination holds the value from the randomDestination();
-console.log(destination);
 
 function randomRestaurant() {
-    let restaurant = ['bahama breeze', 'olive garden', 'cheesecake factory', 'red lobster', 'yard house '];
-    let chosenRestaurant;
-    for(let i = 0; i < restaurant.length; i++) {
-        let rand = Math.floor(Math.random() * ((restaurant.length - 1) - 0 + 1) + 0);
-        chosenRestaurant = restaurant[rand];
-    }
+    let chosenRestaurant = loopArray(restaurant);
     dayTrip.push(chosenRestaurant);
     return chosenRestaurant;
 }
-let restaurant = randomRestaurant();
-console.log(restaurant);
+console.log(randomRestaurant());
+
 
 function randomTransportation() {
-    let transporation = ['car', 'bus', 'shared ride', 'tram', 'flight'];
-    let chosenTranspo;
-    for(let i = 0; i < transporation.length; i++) {
-        let rand = Math.floor(Math.random() * ((transporation.length - 1) - 0 + 1) + 0);
-        chosenTranspo = transporation[rand];
-    }
+    let chosenTranspo = loopArray(transporation);
     dayTrip.push(chosenTranspo);
     return chosenTranspo;
 }
-let transportation = randomTransportation();
-console.log(transportation);
+console.log(randomTransportation());
+
 
 function randomEntertainment() {
-    let entertainment = ['sightsee', 'comedy club', 'museum', 'hike', 'beach'];
-    let chosenEntertainment;
-    for(let i = 0; i < entertainment.length; i++) {
-        let rand = Math.floor(Math.random() * ((entertainment.length - 1) - 0 + 1) + 0);
-        chosenEntertainment = entertainment[rand];
-    }
+    let chosenEntertainment = loopArray(entertainment);
     dayTrip.push(chosenEntertainment);
     return chosenEntertainment;
 }
-let entertainment = randomEntertainment();
-console.log(entertainment);
+console.log(randomEntertainment());
 console.log(dayTrip);
 
 function updateDayTrip(tripUpdate) {
     switch(tripUpdate) {
         case "destination":
-            let updateDes = randomDestination();
+            let updateDestination = loopArray(destination);
+            //remove destination at 0 index from the destination array
             dayTrip.shift();
-            dayTrip.unshift(updateDes);
-            dayTrip.pop();
+            //insert new random destination at 0 index to the destination array
+            dayTrip.unshift(updateDestination);
             console.log(dayTrip);
             break;
         case "restaurant": 
-            let updateRes = randomRestaurant();
-            // .splice() selects index, removes index, and inserts new value
-            // using delete creates empty spots
-            dayTrip.splice(1, 1, updateRes);
-            dayTrip.pop();
+            let updateRestaurant = loopArray(restaurant);
+            // .splice() selects index, removes a number of items starting at given index, and inserts new value
+            // index 1, delete 1, insert updateRestaurant
+            dayTrip.splice(1, 1, updateRestaurant);
+            console.log(dayTrip);
+            break;
+        case "transportation":
+            let updateTransportation = loopArray(transporation);
+            dayTrip.splice(2, 1, updateTransportation);
+            console.log(dayTrip);
+            break;
+        case "entertainment":
+            let updateEntertainment = loopArray(entertainment);
+            dayTrip.pop()
+            dayTrip.push(updateEntertainment);
             console.log(dayTrip);
             break;
         default:
             console.log("no changes made");
     }
 }
+
+
+
+
 
 
